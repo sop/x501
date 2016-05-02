355b84e276e7eaa86d5e907039bb2f464e5930c9
Joni Eskelinen <joni@mobie.fi>
2016-05-02 09:02:48 +0300

    Add caseExactMatch matching rule

---
+++ lib/X501/MatchingRule/CaseExactMatch.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace X501\MatchingRule;
+
+use X501\StringPrep\StringPreparer;
+
+
+/**
+ * Implements 'caseExactMatch' matching rule.
+ *
+ * @link https://tools.ietf.org/html/rfc4517#section-4.2.4
+ */
+class CaseExactMatch extends StringPrepMatchingRule
+{
+	/**
+	 * Constructor
+	 *
+	 * @param int $string_type ASN.1 string type tag
+	 */
+	public function __construct($string_type) {
+		parent::__construct(StringPreparer::forStringType($string_type));
+	}
+}
